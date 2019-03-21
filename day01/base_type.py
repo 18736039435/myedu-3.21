4a278f906e1f16ae9e7b152490c7a9688fdbb095
18736039435 <1281266271@qq.com>
2019-03-21 15:44:37 +0800

    first commit

---
+++ day01/base_type.py
@@ -0,0 +1,14 @@
+def int_demo():
+    aint = 1
+    print(aint)
+
+
+def ad(aint, bint):
+    print(aint)
+    print(bint)
+    return aint + bint
+
+
+if __name__ == '__main__':
+    rest = ad(1, 2)
+    print(rest)
